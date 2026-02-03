cce3280fab73fe63af3cbc0affdfba1bdd50a72f
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2026-02-02 18:49:17 -0700

    Launcher layout

--- src/main/java/us/malfeasant/templatefx/App.java
+++ src/main/java/us/malfeasant/templatefx/App.java
@@ -1,6 +1,11 @@
 package us.malfeasant.templatefx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -10,8 +15,28 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Hello World!");
+        var grid = new GridPane(5.0, 5.0);
+        var charButton = newButton("Character", e -> {});
+        var spriteButton = newButton("Sprite", e -> {});
+        var bitmapButton = newButton("Bitmap", e -> {});
+        var hexButton = newButton("Raw Hex", e -> {});
+        var asmButton = newButton("Assemble", e -> {});
+        var dasmButton = newButton("Disassemble", e -> {});
+
+        grid.addRow(0, charButton, spriteButton);
+        grid.addRow(1, bitmapButton, hexButton);
+        grid.addRow(2, asmButton, dasmButton);
+
+        stage.setTitle("C64 Stuff");
+        stage.setScene(new Scene(grid));
         stage.show();
+    }
+
+    private Button newButton(String text, EventHandler<ActionEvent> e) {
+        var button = new Button(text);
+        button.setMaxWidth(Double.MAX_VALUE);
+        button.setOnAction(e);
+        return button;
     }
 
     public static void main(String[] args) {
