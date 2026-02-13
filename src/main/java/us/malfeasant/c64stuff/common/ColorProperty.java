36ecd3e111f7ef998f38f4aff1ce18ad1a1adc3a
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2026-02-13 00:04:58 -0700

    Beginning common PixelField type

---
+++ src/main/java/us/malfeasant/c64stuff/common/ColorProperty.java
@@ -0,0 +1,6 @@
+package us.malfeasant.c64stuff.common;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.paint.Color;
+
+public class ColorProperty extends SimpleObjectProperty<Color> {}
